9f9e5a9f05260c5bed24ddd8d37ac82e5dff2b95
ben <ben@soprisapps.com>
2020-01-03 08:58:45 -0700

    Add type hints to test.

--- app/tests/test_index.py
+++ app/tests/test_index.py
@@ -1,4 +1,7 @@
-def test_index(client):
+import pytest
+
+
+def test_index(client: pytest.fixture) -> None:
     """Assert that a status code of 200 is returned from /"""
     index = client.get('/')
     assert index.status == '200 OK'
