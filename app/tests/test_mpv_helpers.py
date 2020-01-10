e25d87c8feae8d4aa2604b55512e371c9cdc0fb1
ben <ben@soprisapps.com>
2020-01-10 14:01:30 -0700

    Refactor mp api tests to use class based testing to reduce code repetition and improve organization.

--- app/tests/test_mpv_helpers.py
+++ app/tests/test_mpv_helpers.py
@@ -3,8 +3,9 @@ import encodings
 import pytest
 import requests
 from mysql.connector import CMySQLConnection, Error, MySQLConnection
+from _pytest.monkeypatch import MonkeyPatch
 
-from .test_data.mp_api_response import test_expected_data, test_ticks_response, test_user_data
+from .test_data.mp_api_response import test_expected_data, test_processed_csv, test_ticks_response, test_user_data
 from ..helpers.database_connection import db_connect, db_close
 from ..helpers.mountain_project import MountainProjectHandler
 
@@ -42,53 +43,74 @@ class MockResponse:
         return test_ticks_response
 
 
-def test_prod_env_mp_handler(monkeypatch):
-    """Any arguments may be passed and mocked functions will always return our mocked objects."""
-    def mock_get(*args, **kwargs):
+class TestMountainProjectHandler:
+    @classmethod
+    def setup_class(cls):
+        # Create MountainProjectHandler instances which will use monkeypatch and test data.
+        cls.api_dev = MountainProjectHandler(email="test@example.com", api_key="", dev_env=True)
+        cls.api_prod = MountainProjectHandler(email="test_email@example.com", api_key='test_key', dev_env=False)
+
+        # Apply monkeypatch's
+        cls.monkeypatch = MonkeyPatch()
+        cls.monkeypatch.setattr(requests, "get", cls.mock_get)
+        cls.monkeypatch.setattr(requests.models.Response, "json", cls.mock_json)
+        cls.monkeypatch.setattr(requests.models.Response, "content", cls.mock_content)
+
+    # Any arguments may be passed and mocked functions will always return our mock objects.
+    @staticmethod
+    def mock_get(*args, **kwargs) -> MockResponse:
         # Returns an empty requests.Response object
         return MockResponse()
 
-    def mock_json(*args, **kwargs):
+    @staticmethod
+    def mock_json(*args, **kwargs) -> MockResponse.json:
         # When Response.json() is called, return our test data instead of actually calling that method.
         return MockResponse().json()
 
-    def mock_content(*args, **kwargs):
+    @staticmethod
+    def mock_content(*args, **kwargs) -> MockResponse.content:
         # When Response.content is called return our MockResponse content property.
         return MockResponse().content
 
-    # apply the monkeypatch's
-    monkeypatch.setattr(requests, "get", mock_get)
-    monkeypatch.setattr(requests.models.Response, "json", mock_json)
-    monkeypatch.setattr(requests.models.Response, "content", mock_content)
+    def test_mp_api_user_data(self) -> None:
+        """Confirms fetching and parsing logic of data from Mountain Project API"""
+        self.api_prod.fetch_user()
+        data = self.api_prod.parse_user_data()
 
-    # Create MountainProjectHandler instance which will use monkeypatch and test data.
-    api = MountainProjectHandler(email="test_email@example.com", api_key='test_key', dev_env=False)
-    api.fetch_user()
-    data = api.parse_user_data()
+        # Assertions of returned data from MountainProjectParser.
+        assert data["mp_id"] == 105324100
+        assert data["name"] == "Test User"
+        assert data["status"] == 0
 
-    # Make assertions of returned data from MountainProjectParser.
-    assert data["mp_id"] == 105324100
-    assert data["name"] == "Test User"
-    assert data["status"] == 0
+    def test_mp_api_user_ticks(self) -> None:
+        """Confirms fetching and processing of ticklist csv returns expected output."""
+        self.api_prod.fetch_tick_list()
+        data = self.api_prod.parse_tick_list()
 
-    # Fetch the mock mock tick list
-    api.fetch_tick_list()
-    data = api.parse_tick_list()
+        # Assert data output is what we expect
+        assert data["status"] == 0
+        assert data["data"] == test_expected_data
 
-    # Assert data output is what we expect
-    assert data["status"] == 0
-    assert data["data"] == test_expected_data
+    def test_mp_dev_env_user_data(self):
+        """dev_env=True, so simply assert returned data matches _DEV_USER_DATA"""
+        response = self.api_dev.fetch_user()
+        assert response["name"] == "Dev"
+        assert response["mp_id"] == 1111
 
+    def test_mp_dev_env_parse_user_data(self):
+        """dev_env=True, so simply assert returned data is what we expect from parse_data()"""
+        data = self.api_dev.parse_user_data(dev_env=True)
+        assert data['mp_id'] == 1111
+        assert data["name"] == "Dev"
+        assert data["status"] == 0
 
-def test_dev_env_mp_handler():
-    # dev_env=True, so simply assert returned data matches _DEV_USER_DATA
-    api = MountainProjectHandler(email="test_email@example.com", api_key='test_key', dev_env=True)
-    response = api.fetch_user()
-    assert response["name"] == "Dev"
-    assert response["mp_id"] == 1111
+    def test_mp_dev_env_ticks(self):
+        """When dev is True we expect to return None instead of making a request."""
+        assert self.api_dev.fetch_tick_list() is None
 
-    # assert MountainProjectParser simply returns the desired response.
-    result = api.parse_user_data(dev_env=True)
-    assert result['mp_id'] == 1111
-    assert result["name"] == "Dev"
-    assert result["status"] == 0
+    def test_mp_dev_env_parse_ticks(self):
+        """Ensure when dev_env=True that the processed test_ticks.csv file is the output of parse_tick_list()"""
+        data = self.api_dev.parse_tick_list(dev_env=True)
+        assert data["status"] == 0
+        assert data["data"] == test_processed_csv
+
