f1257ad8fc020cdfe6f9328d8f0cce82bb7c49fb
Zach Wahrer <zachtheclimber@gmail.com>
2020-03-16 14:01:39 -0600

    Fix typos and update error messages for users

--- app/errors/exeptions.py
+++ app/errors/exeptions.py
@@ -1,14 +1,15 @@
 class MPAPIException(Exception):
     status_code = 403
-    msg = "An error occurred fetching data the Mountain Project API. Make sure are using your correct email address, " \
+    msg = "An error occurred fetching data from the Mountain Project API. " \
+        "Make sure you're using a valid, MP registered email address, " \
           "or try your request later."
 
 
 class DatabaseException(Exception):
     status_code = 503
-    msg = "Something went wrong on our end. We are working on a fix, please try again later."
+    msg = "Something went wrong on our end. Please try again later."
 
 
 class RequestException(Exception):
     status_code = 400
-    msg = "Something went wrong on our end. We are working on a fix, please try again later."
+    msg = "Something went wrong on our end. Please try again later."
